b2c67461b61bde2462c6a40ffc28469c1e43f254
Amir Aslam <babaamir325@gmail.com>
2018-11-25 04:55:10 +0530

    updated sitewide urls

--- TechieNest/TechieNest/urls.py
+++ TechieNest/TechieNest/urls.py
@@ -14,8 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
+from blog import urls as blog_urls
+from company import urls as company_urls
+from download import urls as download_urls
+from events import urls as events_urls
+from franchise import urls as franchise_urls
+from jap import urls as jap_urls
+from services import urls as services_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^blog/',include(blog_urls)),
+    re_path(r'^company/',include(company_urls)),
+    re_path(r'^download/',include(download_urls)),
+    re_path(r'^events/',include(events_urls)),
+    re_path(r'^franchise/',include(franchise_urls)),
+    re_path(r'^jap/',include(jap_urls)),
+    re_path(r'^services/',include(services_urls)),
 ]
